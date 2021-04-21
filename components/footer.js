24361e7ade870cefc1dbefa9a2da90068dc36898
Thomas Sankara <thomassankara74@gmail.com>
2021-04-21 09:21:34 +0300

    increase margin on icons

--- components/footer.js
+++ components/footer.js
@@ -14,22 +14,22 @@ const Footer = () => {
       <footer className="px-5 py-10">
         <h2 className="text-center mb-5">Social Media Contacts</h2>
         <ul className="flex items-center justify-center flex-wrap">
-          <li className="mx-5">
+          <li className="mx-5 my-5">
             <FaFacebook className="text-2xl" />
           </li>
-          <li className="mx-5">
+          <li className="mx-5 my-5">
             <FaTwitter className="text-2xl" />
           </li>
-          <li className="mx-5">
+          <li className="mx-5 my-5">
             <FaInstagram className="text-2xl" />
           </li>
-          <li className="mx-5">
+          <li className="mx-5 my-5">
             <FaBehance className="text-2xl" />
           </li>
-          <li className="mx-5">
+          <li className="mx-5 my-5">
             <FaPinterest className="text-2xl" />
           </li>
-          <li className="mx-5">
+          <li className="mx-5 my-5">
             <FaDribbble className="text-2xl" />
           </li>
         </ul>
